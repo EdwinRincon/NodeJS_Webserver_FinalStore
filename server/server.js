b553ca2267c8f8b8fab1430eefb3ae00bd66f823
Edwin Rincon <rinconedwin@hotmail.com>
2021-01-05 02:34:08 +0100

    npm i cors --save

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const express = require('express')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express()
-
+const cors = require('cors')
 const bodyParser = require('body-parser')
+
+// cors-enabled
+app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
